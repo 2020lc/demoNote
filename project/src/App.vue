54c46ecd1e9c9197674caaa818cdfc6c085ad400
liuchen <2542905354@qq.com>
2022-02-28 17:09:33 +0800

    feat: 引入antv/g6

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
+<script>
+export default {
+  methods: {
+    stopJump(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
